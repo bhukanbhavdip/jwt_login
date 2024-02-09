3598e2ca321aa2aee6858c8fc1a3808e0504e889
BHAVDIP BHUKAN <128157200+bhukanbhavdip@users.noreply.github.com>
2024-02-09 10:18:37 +0530

    Add files via upload

---
+++ server/Login.js
@@ -0,0 +1,105 @@
+import React, {useEffect, useState} from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import axios from 'axios'
+import { toast } from 'react-toastify'
+
+const Login = () => {
+    const navigate=useNavigate()
+    const [data, setData] = useState({
+        email: "",
+        password: ""
+    });
+    // const [err,seterr]=useState(false)
+    const handleOnChange=(e)=>{
+        const {name,value} = e.target
+        setData((prev)=>{
+            return {
+                ...prev,
+                [name]: value
+            }
+        })
+    }
+    const handleSubmit= async(e)=>{
+        e.preventDefault();
+        try {
+            const response =  await axios.post("http://localhost:8000/api/login",data)
+            console.log("response",response)
+            if(response.data.error){
+                toast.error(response.data.message);
+                console.log(response.data.message);
+                setData({
+                    email: "",
+                    password: ""
+                })
+                // seterr(true)
+            }
+            if(response.data.success){
+                toast.success(response.data.message);
+                localStorage.setItem("token",response.data.token);
+                navigate("/user-profile")
+            }
+        } catch (error) {
+            if(error.response.status===404){
+                toast.error("User Not Found");
+            }
+            if(error.response.status===401){
+                toast.error(error.response.data.message);
+            }
+        }
+        
+    }
+    console.log("data",data);
+//   useEffect(() => {
+//    if(token){
+//     navigate('/user-profile')
+//    }
+//   }, [])
+  
+
+    return (
+        <>
+            <section className="vh-100 gradient-custom">
+                <div className="container py-5 h-100">
+                    <div className="row d-flex justify-content-center align-items-center h-100">
+                        <div className="col-12 col-md-8 col-lg-6 col-xl-5">
+                            <div className="card bg-dark text-white" style={{borderRadius: "1rem"}}>
+                                <div className="card-body p-5 text-center">
+
+                                    <div className="mb-md-5 mt-md-4 pb-5">
+
+                                        <h2 className="fw-bold mb-2 text-uppercase">Login</h2>
+                                        <p className="text-white-50 mb-5">Please enter your login and password!</p>
+                                        <form onSubmit={handleSubmit}>
+                                        <div className="form-outline form-white mb-4">
+                                            <label className="form-label"   htmlFor="email">Email</label>
+                                            <input type="email" id="email"  value={data.email} name='email' className="form-control form-control-lg" onChange={handleOnChange} />
+                                        </div>
+
+                                        <div className="form-outline form-white mb-4">
+                                            <label className="form-label"   htmlFor="password">Password</label>
+                                            <input type="password" id="password" name='password' value={data.password} className="form-control form-control-lg" onChange={handleOnChange} />
+                                        </div>
+
+                                        <p className="small mb-5 pb-lg-2"><a className="text-white-50" href="#!">Forgot password?</a></p>
+
+                                        <button className="btn btn-outline-light btn-lg px-5" type="submit">Login</button>
+                                        </form>
+
+                                    </div>
+
+                                    <div>
+                                        <p className="mb-0">Don't have an account? <Link to="/" className="text-white-50 fw-bold">Sign Up</Link>
+                                        </p>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default Login
---
+++ server/Signup.js
@@ -0,0 +1,110 @@
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios'
+import { toast } from 'react-toastify';
+const Signup = () => {
+    const navigate = useNavigate()
+    const [data, setData] = useState({
+        name: "",
+        email: "",
+        password: "",
+    })
+    const handleOnChange = (e) => {
+        const { name, value } = e.target
+        setData((prev) => {
+            return {
+                ...prev,
+                [name]: value
+            }
+        })
+    }
+    console.log(data)
+    const handleSubmit = async (e) => {
+
+        e.preventDefault();
+        try {
+            const response = await axios.post("http://localhost:8000/api/sign-up", data)
+            console.log("response", response)
+            if (response.data.success) {
+                toast.success(response.data.message);
+                navigate('/login')
+            }
+            if (response.data.error) {
+                toast.error(response.data.message);
+                setData({
+                    name: "",
+                    email: "",
+                    password: "",
+                })
+            }
+        } catch (error) {
+            if(error.response.status===400){
+                toast.error("Fill All The Data..!");
+            }
+            if(error.response.status===409){
+                toast.error("User Already Exists");
+            }
+            if(error.response.status===401){
+                toast.error(error.response.data.message);
+            }
+        }
+            
+    }
+    return (
+        <>
+            <section className="vh-100 bg-image"
+                style={{ backgroundImage: 'url("https://mdbcdn.b-cdn.net/img/Photos/new-templates/search-box/img4.webp")' }}>
+                <div className="mask d-flex align-items-center h-100 gradient-custom-3">
+                    <div className="container h-100">
+                        <div className="row d-flex justify-content-center align-items-center h-100">
+                            <div className="col-12 col-md-9 col-lg-7 col-xl-6">
+                                <div className="card" style={{ borderRadius: 15 }}>
+                                    <div className="card-body p-5">
+                                        <h2 className="text-uppercase text-center mb-5">Create an account</h2>
+                                        <form onSubmit={handleSubmit}>
+
+                                            <div className="form-outline mb-4">
+                                                <label className="form-label" htmlFor="name" >Your Name</label>
+                                                <input type="text" id="name" name='name' value={data.name} className="form-control form-control-lg" onChange={handleOnChange} />
+                                            </div>
+
+                                            <div className="form-outline form-white mb-4">
+                                                <label className="form-label"  htmlFor="email">Your Email</label>
+                                                <input type="email" id="email" value={data.email} name='email' className="form-control form-control-lg" onChange={handleOnChange} />
+                                            </div>
+
+                                            <div className="form-outline form-white mb-4">
+                                                <label className="form-label" htmlFor="password">Your Password</label>
+                                                <input type="password" id="password" value={data.password} name='password' className="form-control form-control-lg" onChange={handleOnChange} />
+                                            </div>
+
+                                            <div className="form-check d-flex justify-content-center mb-5">
+                                                <input className="form-check-input me-2" type="checkbox" value="" id="form2Example3cg" />
+                                                <label className="form-check-label" htmlFor="form2Example3g">
+                                                    I agree all statements in <u>Terms of service</u>
+                                                </label>
+                                            </div>
+
+                                            <div className="d-flex justify-content-center">
+                                                <button type="submit"
+                                                    className="btn btn-success btn-block btn-lg gradient-custom-4 text-body">Sign Up</button>
+                                            </div>
+
+                                        </form>
+
+                                        <p className="text-center text-muted mt-5 mb-0">Have already an account? <Link to="/login"
+                                            className="fw-bold text-body"><u>Login here</u></Link></p>
+
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default Signup
